7c69f6e1501dfb910d103611db545cdd99339833
Piotr Michna <pm@piotrmichna.pl>
2021-01-29 23:13:07 +0100

    utworzenie podstawowej trasy dla aplikacji botanical

--- wdp/wdp/urls.py
+++ wdp/wdp/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from wdp.views import Main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Main.as_view(), name='main'),
+    path('botanical/', include('botanical.urls')),
 ]
