1f0d3ff7bba98970854f12afafb5af1a27f37993
Piotr Michna <pm@piotrmichna.pl>
2021-01-29 16:58:33 +0100

    test głównego widku

---
+++ wdp/wdp/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.views import View
+
+
+class Main(View):
+    def get(self, request):
+        return HttpResponse(f'''
+        <h1>Znajdź roślinę</h1>
+        <h3><a href="#</a> Katalog roślin</h3>''')
