d391fd99a4d5e5220be5ffe8715ce59899b62c20
Piotr Michna <pm@piotrmichna.pl>
2021-01-30 22:39:56 +0100

    modyfikacja widoku podstawowego dla aplikacji botanical

--- wdp/botanical/views.py
+++ wdp/botanical/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views import View
 
 
 class BotanicalView(View):
     def get(self, request):
-        return HttpResponse('<h1>Botanical</h1>')
+        return render(request, 'botanical_base.html')
