50fe28eab97cafd5d97cdac482ffe0b513694760
Piotr Michna <pm@piotrmichna.pl>
2021-01-30 23:44:53 +0100

    definicja modelu nazw Rodzoju

--- wdp/botanical/models.py
+++ wdp/botanical/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class BotSystGenus(models.Model):
+    lac_name = models.CharField(max_length=32, null=False, verbose_name='Nazwa łacińska')
+    pl_name = models.CharField(max_length=32, null=False, verbose_name='Nazwa polska')
+    hybrid = models.BooleanField(null=True, verbose_name='Mieszaniec')
+
+    def __str__(self):
+        return self.lac_name
+
