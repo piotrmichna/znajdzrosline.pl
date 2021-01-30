0eee128e023b4c123532b45c426a0e8e6745dc0b
Piotr Michna <pm@piotrmichna.pl>
2021-01-30 23:42:31 +0100

    definicja widoku dodawania dodawania roślin

--- wdp/botanical/views.py
+++ wdp/botanical/views.py
@@ -6,3 +6,8 @@ from django.views import View
 class BotanicalView(View):
     def get(self, request):
         return render(request, 'botanical_base.html')
+
+
+class BotanicalAddView(View):
+    def get(self, request):
+        return render(request, 'botanical_add.html')
