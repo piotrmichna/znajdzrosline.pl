2593c382336d1bba3e7867d7752eccb7424e27fc
Piotr Michna <pm@piotrmichna.pl>
2021-01-30 23:55:02 +0100

    definicja modelu nazw Gatunków

--- wdp/botanical/models.py
+++ wdp/botanical/models.py
@@ -9,3 +9,9 @@ class BotSystGenus(models.Model):
     def __str__(self):
         return self.lac_name
 
+
+class BotSystSpecies(models.Model):
+    genus = models.ForeignKey(BotSystGenus, on_delete=models.CASCADE)
+    lac_name = models.CharField(max_length=32, null=False, verbose_name='Nazwa łacińska')
+    pl_name = models.CharField(max_length=32, null=False, verbose_name='Nazwa polska')
+    hybrid = models.BooleanField(null=True, verbose_name='Mieszaniec')
