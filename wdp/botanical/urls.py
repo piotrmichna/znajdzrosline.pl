1493dbd46e10b54f016a1c9b745643a8433ae51b
Piotr Michna <pm@piotrmichna.pl>
2021-01-30 23:43:25 +0100

    definicja trasy dodawanai roślin

--- wdp/botanical/urls.py
+++ wdp/botanical/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from botanical.views import BotanicalView
+from botanical.views import BotanicalView, BotanicalAddView
 
 urlpatterns = [
     path('', BotanicalView.as_view(), name='botanical'),
+    path('add/', BotanicalAddView.as_view(), name='botanical-add'),
 ]
