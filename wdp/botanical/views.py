e83eec49362226132b38b8c963a1abefdf336c25
Piotr Michna <pm@piotrmichna.pl>
2021-01-31 01:13:15 +0100

    definicja widoku dodawania nazw rodzaju

--- wdp/botanical/views.py
+++ wdp/botanical/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+
+from botanical.models import BotSystGenus
 
 
 class BotanicalView(View):
@@ -11,3 +13,37 @@ class BotanicalView(View):
 class BotanicalAddView(View):
     def get(self, request):
         return render(request, 'botanical_add.html')
+
+
+class BotanicalAddGenusView(View):
+    def get(self, request):
+        return render(request, 'botanical_add_genus.html')
+
+    def post(self, request):
+        genus_lac = request.POST.get('genus_lac')
+        genus_pl = request.POST.get('genus_pl')
+        error = []
+        if len(genus_lac) and len(genus_pl):
+            if genus_lac[0] == 'x' or genus_pl[0] == 'x':
+                if genus_lac[0] != 'x' or genus_pl[0] != 'x':
+                    error.append("Dla mieszańca obie nazwy powinna poprzedzać litera x.")
+                else:
+                    lac = genus_lac.split(' ')
+                    pl = genus_pl.split(' ')
+                    if not BotSystGenus.objects.filter(lac_name=lac[1], pl_name=pl[1], hybrid=True).count():
+                        BotSystGenus.objects.create(lac_name=lac[1], pl_name=pl[1], hybrid=True)
+                        return redirect('/botanical/add/')
+                    else:
+                        error.append('W katalogu już istniej Rodzaj o takich nazwach.')
+            else:
+                if not BotSystGenus.objects.filter(lac_name=genus_lac, pl_name=genus_pl).count():
+                    BotSystGenus.objects.create(lac_name=genus_lac, pl_name=genus_pl, hybrid=False)
+                    return redirect('/botanical/add/')
+                else:
+                    error.append('W katalogu już istniej Rodzaj o takich nazwach.')
+        else:
+            error.append('Obie nazwy są wymagane, jeśli polska nazwa nie istnieje to wpisz łacińską z małej litery.')
+
+        return render(request, 'botanical_add_genus.html', {'error': error,
+                                                            'genus_lac': genus_lac,
+                                                            'genus_pl': genus_pl})
