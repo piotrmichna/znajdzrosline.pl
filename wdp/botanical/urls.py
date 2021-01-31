9c49c16f3b356f75df62798521060bf3f6619c21
Piotr Michna <pm@piotrmichna.pl>
2021-01-31 01:12:07 +0100

    definicja trasy dodawania nazw rodzaju

--- wdp/botanical/urls.py
+++ wdp/botanical/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from botanical.views import BotanicalView, BotanicalAddView
+from botanical.views import (BotanicalView, BotanicalAddView, BotanicalAddGenusView)
 
 urlpatterns = [
     path('', BotanicalView.as_view(), name='botanical'),
     path('add/', BotanicalAddView.as_view(), name='botanical-add'),
+    path('add/genus/', BotanicalAddGenusView.as_view(), name='botanical-add-genus'),
 ]
