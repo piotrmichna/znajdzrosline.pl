66003a983e48b9123aa2bce8d21e26fd78cacbc6
Piotr Michna <pm@piotrmichna.pl>
2021-02-01 22:02:08 +0100

    definicja widoku login i logout

--- wdp/accounts/views.py
+++ wdp/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'registration/login.html')
+
+    if request.method == "POST":
+        error = []
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/botanical/')
+            else:
+                error.append("Nie znaleziono urzytkownika")
+                return render(request, 'registration/login.html', {'username': username,
+                                                                   'password': password,
+                                                                   'error': error})
+        else:
+            error.append("Nie wypełniono wszystkich pól.")
+            return render(request, 'registration/login.html', {'username': username,
+                                                               'password': password,
+                                                               'error': error})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
