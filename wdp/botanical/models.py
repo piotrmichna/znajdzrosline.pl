4cedf34607fed868023904cdcc0b49cd0dd127e7
Piotr Michna <pm@piotrmichna.pl>
2021-02-02 00:43:39 +0100

    definicja głównego model ROŚLINY

--- wdp/botanical/models.py
+++ wdp/botanical/models.py
@@ -25,3 +25,12 @@ class BotSystCultivar(models.Model):
 class PlantBodyType(models.Model):
     body_type = models.CharField(max_length=32, null=False)
     lp = models.PositiveSmallIntegerField(default=0)
+
+
+class PlntLibraries(models.Model):
+    genus = models.ForeignKey(BotSystGenus, on_delete=models.CASCADE)
+    species = models.ForeignKey(BotSystSpecies, on_delete=models.CASCADE, null=True)
+    cultivar = models.ForeignKey(BotSystCultivar, on_delete=models.CASCADE, null=True)
+    body_type = models.OneToOneField(PlantBodyType, on_delete=models.CASCADE, null=True)
+    edible = models.BooleanField(default=False)
+    # plant= PlnatLibraries.objects.filter(body_type__body_type='Drzewo iglaste', genus__lac_name='Acer')
