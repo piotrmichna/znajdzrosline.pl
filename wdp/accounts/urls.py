548bb02d33374cad44742837bb3b2949af244804
Piotr Michna <pm@piotrmichna.pl>
2021-02-01 22:58:47 +0100

    trasy rejestracji

--- wdp/accounts/urls.py
+++ wdp/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, sign_up, logout_view, login_view
+from .views import index, signup_view, logout_view, login_view
 
 urlpatterns = [
     path('', index, name="home"),
+    path('signup/', signup_view, name="signup"),
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
 ]
