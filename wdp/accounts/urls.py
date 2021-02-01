b73178284cd6b7b1dab49121eac4ba74dbb1c755
Piotr Michna <pm@piotrmichna.pl>
2021-02-01 22:02:56 +0100

    dodanie tras login i logout

--- wdp/accounts/urls.py
+++ wdp/accounts/urls.py
@@ -1,2 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import index, sign_up, logout_view, login_view
 
-urlpatterns = []
+urlpatterns = [
+    path('', index, name="home"),
+    path('login/', login_view, name="login"),
+    path('logout/', logout_view, name="logout"),
+]
