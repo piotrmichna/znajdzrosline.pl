c2c2fdd754e8c8cdf072ba2f8b9827861eb971c9
Piotr Michna <pm@piotrmichna.pl>
2021-02-02 21:22:21 +0100

    trasa do widoku usuwania rosliny

--- wdp/botanical/urls.py
+++ wdp/botanical/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from botanical.views import (BotanicalView, BotanicalAddView, BotanicalAddGenusView, BotanicalAddSpeciesView,
                              BotanicalAddClear, BotanicalTypeAddView, BotanicalAddTypeView, BotanicalPlantShowView,
-                             BotanicalPlantEditView)
+                             BotanicalPlantEditView, BotanicalPlantDeleteView)
 
 urlpatterns = [
     path('', BotanicalView.as_view(), name='botanical'),
     path('show/<int:plant_id>/', BotanicalPlantShowView.as_view(), name='botanical-show'),
     path('edit/<int:plant_id>/', BotanicalPlantEditView.as_view(), name='botanical-edit'),
+    path('delete/<int:plant_id>/', BotanicalPlantDeleteView.as_view(), name='botanical-delete'),
     path('add/', BotanicalAddView.as_view(), name='botanical-add'),
     path('addtype/', BotanicalTypeAddView.as_view(), name='botanical-type-add'),
     path('add/clear/', BotanicalAddClear.as_view(), name='botanical-add-clear'),
