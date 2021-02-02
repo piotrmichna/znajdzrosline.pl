57d4c203d8c8b94f65cd21f387c3092193b9bab4
Piotr Michna <pm@piotrmichna.pl>
2021-02-02 19:20:53 +0100

    dodanie modelu opisow roslin

--- wdp/botanical/models.py
+++ wdp/botanical/models.py
@@ -36,10 +36,24 @@ class PlantBodyType(models.Model):
         return self.body_type
 
 
+class PlantDescriptions(models.Model):
+    botanical = models.TextField()
+    cultivation = models.TextField()
+    destiny = models.TextField()
+
+
 class PlntLibraries(models.Model):
     genus = models.ForeignKey(BotSystGenus, on_delete=models.CASCADE)
     species = models.ForeignKey(BotSystSpecies, on_delete=models.CASCADE, null=True)
     cultivar = models.ForeignKey(BotSystCultivar, on_delete=models.CASCADE, null=True)
     body_type = models.ForeignKey(PlantBodyType, on_delete=models.CASCADE, null=True)
     edible = models.BooleanField(default=False)
+    description = models.ForeignKey(PlantDescriptions, on_delete=models.CASCADE, null=True)
+
     # plant= PlnatLibraries.objects.filter(body_type__body_type='Drzewo iglaste', genus__lac_name='Acer')
+
+    def edible_show(self):
+        if self.edible:
+            return 'Tak'
+        else:
+            return 'Nie'
