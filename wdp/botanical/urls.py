0bb7e83c66ac6e1f0f54260982ba0f4b93c7ac1a
Piotr Michna <pm@piotrmichna.pl>
2021-02-02 19:50:37 +0100

    trasa widoku edycji rosliny

--- wdp/botanical/urls.py
+++ wdp/botanical/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from botanical.views import (BotanicalView, BotanicalAddView, BotanicalAddGenusView, BotanicalAddSpeciesView,
-                             BotanicalAddClear, BotanicalTypeAddView, BotanicalAddTypeView, PlanicalPlantShowView)
+                             BotanicalAddClear, BotanicalTypeAddView, BotanicalAddTypeView, BotanicalPlantShowView,
+                             BotanicalPlantEditView)
 
 urlpatterns = [
     path('', BotanicalView.as_view(), name='botanical'),
-    path('show/<int:plant_id>/', PlanicalPlantShowView.as_view(), name='botanical-show'),
+    path('show/<int:plant_id>/', BotanicalPlantShowView.as_view(), name='botanical-show'),
+    path('edit/<int:plant_id>/', BotanicalPlantEditView.as_view(), name='botanical-edit'),
     path('add/', BotanicalAddView.as_view(), name='botanical-add'),
     path('addtype/', BotanicalTypeAddView.as_view(), name='botanical-type-add'),
     path('add/clear/', BotanicalAddClear.as_view(), name='botanical-add-clear'),
