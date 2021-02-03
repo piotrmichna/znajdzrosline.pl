dd9d7e03ec534e557fcb8170a6ebac1561b6dbff
Piotr Michna <pm@piotrmichna.pl>
2021-02-03 16:53:19 +0100

    trasa widoku edycji pokroju

--- wdp/botanical/urls.py
+++ wdp/botanical/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from botanical.views import (BotanicalView, BotanicalAddView, BotanicalAddGenusView, BotanicalAddSpeciesView,
                              BotanicalAddClear, BotanicalTypeAddView, BotanicalAddTypeView, BotanicalPlantShowView,
-                             BotanicalPlantEditView, BotanicalPlantDeleteView, BotanicalListView, PlantEditDescriptions)
+                             BotanicalPlantEditView, BotanicalPlantDeleteView, BotanicalListView, PlantEditDescriptions,
+                             PlantEditBody)
 
 urlpatterns = [
     path('', BotanicalView.as_view(), name='botanical'),
@@ -10,7 +11,7 @@ urlpatterns = [
     path('show/<int:plant_id>/', BotanicalPlantShowView.as_view(), name='botanical-show'),
     path('edit/<int:plant_id>/', BotanicalPlantEditView.as_view(), name='botanical-edit'),
     path('edit-descriptions/<int:plant_id>/', PlantEditDescriptions.as_view(), name='botanical-edit-descriptions'),
-
+    path('edit-body/<int:plant_id>/', PlantEditBody.as_view(), name='botanical-edit-body'),
     path('delete/<int:plant_id>/', BotanicalPlantDeleteView.as_view(), name='botanical-delete'),
     path('add/', BotanicalAddView.as_view(), name='botanical-add'),
     path('addtype/', BotanicalTypeAddView.as_view(), name='botanical-type-add'),
