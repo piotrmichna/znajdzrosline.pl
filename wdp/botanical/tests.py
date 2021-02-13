a4ffd7cb07157f32d036878f791ebccec1e2842a
Piotr Michna <pm@piotrmichna.pl>
2021-02-13 17:46:34 +0100

    testy pytest

--- wdp/botanical/tests.py
+++ wdp/botanical/tests.py
@@ -1,113 +1,99 @@
 import pytest
-from django.contrib.auth.models import User, Permission
-from django.test import TestCase, Client
-from django.urls import reverse
-import conftest
-from botanical.models import BotSystGenus, BotSystSpecies, BotSystCultivar, PlantBodyType, PlantDescriptions, \
-    PlntLibraries
+from django.test import Client
+from botanical.models import BotSystGenus, BotSystSpecies, PlantBodyType, PlntLibraries
 
 
 @pytest.mark.django_db
-def test_post_no_authorized_genus_view(client):
-    User.objects.create_user('test2')
+def test_klient_user_login(klient):
+    klient
+    c = Client()
+    response = c.post('/accounts/login/', {'username': 'Klient', })
+    assert response.status_code == 200
 
-    client.force_login('test2')
-    response = client.post('/botanical/add/genus/', {'lac_name': 'Fraxinus', 'pl_name': 'jesion'})
+
+@pytest.mark.django_db
+def test_klient_user_add_genus(klient):
+    c = Client()
+    c.force_login(klient)
+    response = c.post('/botanical/add/genus/', {'lac_name': 'Acer', 'pl_name': 'klon'})
     assert response.status_code == 403
 
-# @pytest.mark.django_db
-# def test_post_authorized_genus_view(client):
-#     user = User.objects.create_user(username='test', password='TestPass')
-#     permission = Permission.objects.get(codename='add_botsystgenus')
-#     user.user_permissions.add(permission)
-#     user.save()
-#
-#     client.login(username='test', password='TestPass')
-#     response = client.post('/botanical/add/genus/', {'lac_name': 'Fraxinus', 'pl_name': 'jesion'})
-#
-#     assert BotSystGenus.objects.get(lac_name='Fraxinus')
-#
-#
-# @pytest.mark.django_db
-# def test_post_authorized_species_view(client):
-#     """TEST DODAWANIA NAZW GATUNKÓW"""
-#     user = User.objects.create_user(username='test', password='TestPass')
-#     permission = Permission.objects.get(codename='add_botsystgenus')
-#     user.user_permissions.add(permission)
-#     user.save()
-#
-#     client.login(username='test', password='TestPass')
-#     response = client.post('/botanical/add/species/', {'lac_name': 'excelsrior', 'pl_name': 'wyniosły'})
-#
-#     assert BotSystGenus.objects.get(lac_name='excelsrior')
-#
-#
-# @pytest.mark.django_db
-# def test_authorized_botanical_add_type_view(client):
-#     """
-#     Test BotanicalAddTypeView
-#
-#     - sprawdzenie poprawności dodawania unikalnych nazw typów rośliny
-#     _ sprawdzenie zwracania komunikatu error po ponownym dodaniu tej samej rośliny
-#     """
-#     user = User.objects.create_user(username='test', password='TestPass')
-#     permission = Permission.objects.get(codename='add_botsystgenus')
-#     user.user_permissions.add(permission)
-#     user.save()
-#     client.login(username='test', password='TestPass')
-#
-#     response = client.post('/botanical/add/type/', {'body_type': 'Rośliny zielne'})
-#     assert PlantBodyType.objects.get(body_type='Rośliny zielne')
-#
-#     response = client.post('/botanical/add/type/', {'body_type': 'Rośliny zielne'})
-#     assert len(response.error) > 0
-#
-#
-# @pytest.mark.django_db
-# def test_authorized_botanical_syst_add_view(client):
-#     """
-#     Test BotanicalSystAddView
-#
-#     sprawdzenie zmiennej sesyjnej ustawianej po wyborze gatunku
-#     """
-#     user = User.objects.create_user(username='test', password='TestPass')
-#     permission = Permission.objects.get(codename='add_botsystgenus')
-#     user.user_permissions.add(permission)
-#     user.save()
-#
-#     client.login(username='test', password='TestPass')
-#     response = client.post('/botanical/add/', {'genus_name': 'Acer'})
-#     assert 'Acer' == response.session.get('genus_name')
-#
-#     response = client.post('/botanical/add/', {'species_name': 'platanus'})
-#     assert 'platanus' == response.session.get('species_name')
-#
-#     response = client.post('/botanical/add/', {'cultivar_name': 'Globosum'})
-#     assert 'Globosum' == response.session.get('cultivar_name')
-#
-#
-# @pytest.mark.django_db
-# def test_authorized_plant_edit_descriptions_view(client):
-#     """
-#     Test PlantEditDescriptions
-#
-#     - sprawdzenie poprawności edytowania opisów rośliny
-#     """
-#     user = User.objects.create_user(username='test', password='TestPass')
-#     permission = Permission.objects.get(codename='add_botsystgenus')
-#     user.user_permissions.add(permission)
-#     user.save()
-#
-#     genus = BotSystGenus.objects.create(lac_name='Acer', pl_name='klon', hybrid=False)
-#     species = BotSystSpecies.objects.create(genus=genus, lac_name='platanoides', pl_name='zwyczajny', hybrid=False)
-#     body_type = PlantBodyType.objects.create(body_type='Drzewa', lp=1)
-#
-#     plant = PlntLibraries.objects.create(genus=genus, species=species, body_type=body_type, edible=False)
-#
-#     client.login(username='test', password='TestPass')
-#
-#     response = client.post(f'/botanical/edit-descriptions/{plant.id}/', {'botanical': 'Opis botaniczny',
-#                                                                          'agree': 'agree1'})
-#     plant2 = PlntLibraries.objects.get(id=plant.id)
-#     descr = PlantDescriptions.objects.get(id=plant2.description.id)
-#     assert 'Opis botaniczny' == descr.botanical
+
+@pytest.mark.django_db
+def test_projektant_user_add_genus(projektant):
+    c = Client()
+    c.force_login(projektant)
+    response = c.post('/botanical/add/genus/', {'genus_lac': 'Acer', 'genus_pl': 'klon'})
+    assert response.status_code == 302
+    genus = BotSystGenus.objects.get(lac_name='Acer')
+    assert genus.lac_name == 'Acer'
+
+
+@pytest.mark.django_db
+def test_klient_user_add_species(klient):
+    c = Client()
+    c.force_login(klient)
+    response = c.post('/botanical/add/species/', {'lac_name': 'platanoides', 'pl_name': 'zwyczajny'})
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_projektant_user_add_species(projektant):
+    c = Client()
+    c.force_login(projektant)
+    genus = BotSystGenus.objects.create(lac_name='Acer', pl_name='klon')
+    session = c.session
+    session['genus_name'] = genus.lac_name
+    session.save()
+    assert c.session.get('genus_name')
+    response = c.post('/botanical/add/species/', {'species_lac': 'platanoides', 'species_pl': 'zwyczajny'})
+    assert BotSystSpecies.objects.get(lac_name='platanoides')
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_klient_user_add_body_type(klient):
+    c = Client()
+    c.force_login(klient)
+    response = c.post('/botanical/add/type/', {'body_type': 'Drzewa liściaste', 'lp': 1})
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_projektant_user_add_body_type(projektant):
+    c = Client()
+    c.force_login(projektant)
+    response = c.post('/botanical/add/type/', {'body_type': 'Drzewa liściaste', 'lp': 1})
+    body = PlantBodyType.objects.get(body_type='Drzewa liściaste')
+    assert body.body_type == 'Drzewa liściaste'
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_klient_user_add_plant(klient):
+    c = Client()
+    c.force_login(klient)
+    response = c.post('/botanical/addtype/')
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_projektant_user_add_plant(projektant):
+    c = Client()
+    c.force_login(projektant)
+    genus = BotSystGenus.objects.create(lac_name='Acerx', pl_name='klon')
+    species = BotSystSpecies.objects.create(genus=genus, lac_name='platanoides', pl_name='zwyczajny')
+    body_type = PlantBodyType.objects.create(body_type='Drzewa liściaste', lp=1)
+
+    session = c.session
+    session['genus_name'] = genus.lac_name
+    session['species_name'] = species.lac_name
+    session.save()
+    plant = PlntLibraries.objects.create(genus=genus,
+                                         species=species,
+                                         cultivar=None,
+                                         body_type=body_type,
+                                         edible=False)
+    response = c.post('/botanical/addtype/', {'body_type': 'Drzewa liściaste', 'edible': 'Nie'})
+    assert PlntLibraries.objects.get(id=plant.id)
+    assert response.status_code == 302
--- wdp/conftest.py
+++ wdp/conftest.py
@@ -1,8 +1,15 @@
 import pytest
-from django.test import Client
+from django.contrib.auth.models import User, Permission
 
 
 @pytest.fixture
-def client():
-    client = Client()
-    return client
+def klient():
+    klient = User.objects.create_user(username='Klient', email='em@ail.com', password='ddd')
+    return klient
+
+@pytest.fixture
+def projektant():
+    projektant = User.objects.create_user(username='Projektant', email='em@ail.com', password='ddd')
+    permission = Permission.objects.get(codename='add_botsystgenus')
+    projektant.user_permissions.add(permission)
+    return projektant
